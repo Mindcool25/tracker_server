c5523d14f1a3fe5a1ccb32f0a81fe92b8e589c6f
mindcool24 <thezachm24@gmail.com>
2023-09-23 15:07:37 -0600

    Finished up implementation of announce response, need to fix it tho

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,8 @@
-use crate::response;
+use crate::{
+    requests::AnnounceRequest,
+    response::{self, Peers},
+};
 use redis::AsyncCommands;
-use std::net::Ipv4Addr;
 
 // Data I need:
 //
@@ -10,12 +12,15 @@ use std::net::Ipv4Addr;
 // Maybe use a set for peers for a given hash? something like hash: [ip:port, ip:port, ...], then it can be quickly searched?
 // Maybe optimize to give downloaders the best seeders? or at least the ones with them being done.... Maybe a seeders set and a leechers set?
 // <hash>:seeders and <hash>:leechers, then just <hash> for all peers?
+//
+// Maybe closer to leechers:<hash> and seeders:<hash>, which both store things as <ip>:<port>?
+// Then peer:<connection_id> which holds downloaded:<hash> <dl> left:<hash> <ldl> uploaded:<hash> <up>?
 
 pub fn get_client() -> redis::Client {
     redis::Client::open("redis://127.0.0.1/").unwrap()
 }
 
-pub async fn set_json(r: &response::ConnectResponse) {
+pub async fn set_json() {
     // Connecting to redis
     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
     let mut conn = client.get_async_connection().await.unwrap();
@@ -33,61 +38,92 @@ pub async fn check_hash(hash: [u8; 20]) -> bool {
     let client = get_client();
     let mut conn = client.get_async_connection().await.unwrap();
     let exists: i32 = redis::cmd("EXISTS")
-        .arg(&[format!("{:?}:leechers", hash)])
+        .arg(&[format!("leechers:{:?}", hash)])
         .query_async(&mut conn)
         .await
         .unwrap();
     exists == 1
 }
 
-pub async fn get_peers(hash: [u8; 20], amount: i32) -> response::Peers {
-    // Setting up connection
+pub async fn create_torrent_hash(request: &AnnounceRequest) {
     let client = get_client();
     let mut conn = client.get_async_connection().await.unwrap();
-
-    // Set up return value
-    let mut return_peers: response::Peers = response::Peers { peers: Vec::new() };
-
-    // Setting amount to usize and such
-    let peer_amount: usize;
-    if amount == -1 {
-        peer_amount = 80;
+    if request.left == 0 {
+        let _: () = conn
+            .sadd(
+                format!("seeders:{:?}", request.info_hash), // HACK: make sure to change this back to the actual hash.
+                format!("{:?}:{}", request.ip_address, request.port),
+            )
+            .await
+            .unwrap();
     } else {
-        peer_amount = usize::try_from(amount).unwrap();
+        let _: () = conn
+            .sadd(
+                format!("leechers:{:?}", request.info_hash),
+                format!("{:?}:{}", request.ip_address, request.port),
+            )
+            .await
+            .unwrap();
     }
-
-    // Get seeders, if that isn't enough, get leechers as well.
-    let mut db_peers: Vec<String> = conn.smembers(format!("{:?}:seeders", hash)).await.unwrap();
-    if db_peers.len() < usize::try_from(peer_amount).unwrap() {
-        db_peers = [
-            db_peers,
-            conn.smembers(format!("{:?}:leechers", hash)).await.unwrap(),
-        ]
-        .concat();
-    }
-    // Check if peer still exists here?
-    // Add all peers to Peers struct and retrun
-    // TODO: Maybe look at this and change to only using u32?
-    for db_peer in db_peers {
-        let address: Vec<&str> = db_peer.split(":").collect();
-        let ip_address: Ipv4Addr = address[0].parse().unwrap();
-        let port: u16 = address[1].parse().unwrap();
-        return_peers.peers.push(response::Peer { ip_address, port })
-    }
-
-    // Trim down to correct size and return
-    if return_peers.peers.len() > usize::try_from(amount).unwrap() {
-        return_peers.peers.drain(peer_amount..);
-    }
-    return_peers
 }
 
-// TODO: Make a function that can convert u32 to ipv4 and vice versa
-pub async fn add_peer(hash: [u8; 20], ip: u32, port: u16) {
-    // Connecting to db
+pub async fn get_leecher_count(request: &AnnounceRequest) -> i64 {
+    let client = get_client();
+    let mut conn = client.get_async_connection().await.unwrap();
+    let leechers: i64 = conn
+        .scard(format!("leechers:{:?}", request.info_hash))
+        .await
+        .unwrap();
+    println!("Leechers: {}", leechers);
+    leechers
+}
+
+pub async fn get_seeder_count(request: &AnnounceRequest) -> i64 {
+    let client = get_client();
+    let mut conn = client.get_async_connection().await.unwrap();
+    let seeders: i64 = conn
+        .scard(format!("seeders:{:?}", request.info_hash))
+        .await
+        .unwrap();
+    println!("Seeders: {}", seeders);
+    seeders
+}
+
+pub async fn get_peers(request: &AnnounceRequest) -> Peers {
     let client = get_client();
     let mut conn = client.get_async_connection().await.unwrap();
 
-    // Adding new set as a peer, then give it hash? Do I need to keep track of stuff at all or do I bs it?
-    let _: () = conn.sadd(format!("{}:{}", ip, port), "").await.unwrap();
+    // Setting amount of peers wanted
+    let peer_amount: usize;
+    if request.num_want == -1 {
+        peer_amount = 80;
+    } else {
+        peer_amount = usize::try_from(request.num_want).unwrap();
+    }
+
+    // Getting seeders, if not enough seeders, get some leechers as well
+    let mut db_peers: Vec<String> = conn
+        .smembers(format!("seeders:{:?}", request.info_hash))
+        .await
+        .unwrap();
+    if db_peers.len() < peer_amount {
+        let mut db_leechers: Vec<String> = conn
+            .smembers(format!("leechers:{:?}", request.info_hash))
+            .await
+            .unwrap();
+        db_peers.append(&mut db_leechers);
+    }
+
+    // Creating the return Peers struct
+    let mut return_peers: response::Peers = response::Peers { peers: Vec::new() };
+
+    // Change the vectors of strings to vector of peers
+    for db_peer in db_peers {
+        let addr: Vec<&str> = db_peer.split(":").collect();
+        let ip_address: u32 = addr[0].parse().unwrap();
+        let port: u16 = addr[1].parse().unwrap();
+        return_peers.peers.push(response::Peer { ip_address, port })
+    }
+    println!("Sending {} peers", &return_peers.peers.len());
+    return_peers
 }
--- src/main.rs
+++ src/main.rs
@@ -22,39 +22,62 @@ async fn main() -> io::Result<()> {
     loop {
         // Accept connection and figure out what request the client sent
         let (_len, addr) = sock.recv_from(&mut buf).await?;
-        let request: requests::Request = requests::Request::from_bytes(buf);
-
+        println!("Conn from {}", addr);
+        let mut request: requests::Request = requests::Request::from_bytes(buf);
+        // Get IP, set it to a u32, if IPv6, tell them that it isn't supported.
+        let conn_ip = match addr.ip() {
+            std::net::IpAddr::V4(ip4) => u32::from_be_bytes(ip4.octets()),
+            _ => 0,
+        };
+        if conn_ip == 0 {
+            request.action = 3;
+        }
         let resp = match request.action {
-            0 => handle_connect(request).await,  // Connect
-            1 => handle_announce(request).await, // Announce
-            2 => handle_scrape(request).await,   // Scrape
+            0 => handle_connect(request).await,           // Connect
+            1 => handle_announce(request, conn_ip).await, // Announce
+            2 => handle_scrape(request).await,            // Scrape
+            3 => handle_error(request, "Invalid IP".to_string()).await,
             _ => handle_error(request, "Invalid action".to_string()).await,
         };
-        println!("Response: {:?}", resp);
         sock.send_to(&resp, addr).await?;
     }
 }
 
 async fn handle_connect(request: requests::Request) -> Vec<u8> {
     let conn: requests::ConnectRequest = request.to_connect_request();
-    println!("{:?}", conn);
     let mut rng = rand::thread_rng();
     let resp: response::ConnectResponse = response::ConnectResponse {
         transaction_id: conn.transaction_id,
         connection_id: rng.gen(),
     };
-    db::set_json(&resp).await;
+    println!("{:?}", resp);
     resp.to_bytes()
 }
 
-async fn handle_announce(request: requests::Request) -> Vec<u8> {
+async fn handle_announce(request: requests::Request, ip: u32) -> Vec<u8> {
     // Check if hash exists in database
     // If not, create new set, add peer to set
     // If yes, check set, get max seeder peers, then max leecher peers until number
     // of peers wanted is reached or peers run out
     // Return whatever is left
-    println!("{:?}", request.to_announce_request());
-    Vec::new()
+    let mut req: requests::AnnounceRequest = request.to_announce_request();
+    println!("{:?}", req);
+    if req.ip_address == 0 {
+        req.ip_address = ip;
+    }
+    db::create_torrent_hash(&req).await;
+    //db::get_count(req).await;
+    let s_count: i32 = i32::try_from(db::get_seeder_count(&req).await).expect("oh no");
+    let l_count: i32 = i32::try_from(db::get_leecher_count(&req).await).expect("oh no");
+    let resp = response::AnnounceResponse {
+        transaction_id: req.transaction_id,
+        interval: 5,
+        leechers: l_count,
+        seeders: s_count,
+        peers: db::get_peers(&req).await,
+    };
+    println!("{:?}", resp);
+    resp.to_bytes()
 }
 
 async fn handle_scrape(request: requests::Request) -> Vec<u8> {
